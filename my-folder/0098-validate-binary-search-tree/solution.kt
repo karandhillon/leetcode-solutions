0a80a5fc15047a906f197c7decf7cf8dcb434bdc
Karan Dhillon <me@kdhillon.com>
2021-01-11 04:28:37 +0000

    [LeetCode Sync] Runtime - 432 ms (5.82%), Memory - 40.6 MB (100.00%)

---
+++ my-folder/0098-validate-binary-search-tree/solution.kt
@@ -0,0 +1,29 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun isValidBST(root: TreeNode?): Boolean {
+        return validate(root, null, null)
+    }
+    
+    fun validate(node: TreeNode?, min: Int?, max: Int?): Boolean {
+        if(node == null) return true
+        if(
+            (min != null && node.`val` <= min) ||
+            (max != null && node.`val` >= max)
+        ) return false
+        
+        if(
+            !validate(node.left, min, node.`val`) ||
+            !validate(node.right, node.`val`, max)
+        ) return false
+        else return true
+    } 
+}
