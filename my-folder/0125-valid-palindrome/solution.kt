3b87b6541f50bd070731bcf04347e1be46bae656
Karan Dhillon <me@kdhillon.com>
2021-10-26 15:04:40 +0000

    [LeetCode Sync] Runtime - 1628 ms (5.06%), Memory - 39.6 MB (100.00%)

--- my-folder/0125-valid-palindrome/solution.kt
+++ my-folder/0125-valid-palindrome/solution.kt
@@ -1,10 +1,18 @@
 class Solution {
     fun isPalindrome(s: String): Boolean {
-        val regex = Regex("[^A-Za-z0-9 ]")
-        val string = s.replace(regex, "")
-            .replace(" ", "")
-            .toLowerCase()
-        
-        if(string == string.reversed()) return true else return false
+    val string = s.replace(Regex("[^A-Za-z0-9]"), "").toLowerCase()
+
+    var begin = 0
+    var end = string.length - 1
+    var flag = true
+
+    while (begin < end) {
+        if (string.toCharArray()[begin] != string.toCharArray()[end]) flag = false
+
+        ++begin
+        --end
     }
+
+    return flag
+}
 }
