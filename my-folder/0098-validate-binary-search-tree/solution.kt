da96ccb63782b6899898b6e9d0ffe9a7a963e39e
Karan Dhillon <me@kdhillon.com>
2021-10-26 20:40:12 +0000

    [LeetCode Sync] Runtime - 192 ms (5.82%), Memory - 36.1 MB (100.00%)

--- my-folder/0098-validate-binary-search-tree/solution.kt
+++ my-folder/0098-validate-binary-search-tree/solution.kt
@@ -9,21 +9,35 @@
  * }
  */
 class Solution {
-    fun isValidBST(root: TreeNode?): Boolean {
-        return validate(root, null, null)
+fun isValidBST(root: TreeNode?): Boolean {
+    val stack = Stack<TreeNode>()
+    val output = ArrayList<Int>()
+    var current: TreeNode?
+    var flag = true
+
+    return if (root == null) flag
+    else {
+        current = root
+
+        while (current != null || stack.isNotEmpty()) {
+            while (current != null) {
+                stack.push(current)
+                current = current.left
+            }
+
+            current = stack.pop()
+            output.add(current.`val`)
+            current = current.right
+        }
+
+        val array = output.toIntArray()
+        for (i in 0..array.size-2) {
+            if (array[i] >= array[i+1]) {
+                flag = false
+            }
+        }
+
+        return flag
     }
-    
-    fun validate(node: TreeNode?, min: Int?, max: Int?): Boolean {
-        if(node == null) return true
-        if(
-            (min != null && node.`val` <= min) ||
-            (max != null && node.`val` >= max)
-        ) return false
-        
-        if(
-            !validate(node.left, min, node.`val`) ||
-            !validate(node.right, node.`val`, max)
-        ) return false
-        else return true
-    } 
+}
 }
