2a18da0d31a61c1827e3ced485177116eae82c06
Karan Dhillon <me@kdhillon.com>
2021-10-31 06:18:36 +0000

    [LeetCode Sync] Runtime - 156 ms (5.28%), Memory - 34 MB (100.00%)

--- my-folder/0094-binary-tree-inorder-traversal/solution.kt
+++ my-folder/0094-binary-tree-inorder-traversal/solution.kt
@@ -1,8 +1,25 @@
 class Solution {
     fun inorderTraversal(root: TreeNode?): List<Int> {
-        return bfs(root)
+        return dfs(root)
     }
 
+    fun dfs(root: TreeNode?): List<Int> {
+        val output = ArrayList<Int>()
+        
+        dfsHelper(root, output)
+            
+        return output
+    }
+    
+    fun dfsHelper(node: TreeNode?, output: ArrayList<Int>) {
+        if (node == null) return
+        else {
+            node.left?.let { dfsHelper(it, output) }
+            output.add(node.`val`)
+            node.right?.let { dfsHelper(it, output) }
+        }
+    }
+    
     fun bfs(root: TreeNode?): List<Int> {
         val stack = Stack<TreeNode>()
         val output = ArrayList<Int>()
