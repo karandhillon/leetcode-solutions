bf7d9859ce9386f7ab2ffb927a338c2ac4e7a6eb
Karan Dhillon <me@kdhillon.com>
2021-12-06 02:25:25 +0000

    [LeetCode Sync] Runtime - 180 ms (10.67%), Memory - 37.6 MB (100.00%)

---
+++ my-folder/0236-lowest-common-ancestor-of-a-binary-tree/solution.kt
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int = 0) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+      if (root == null) return null
+      if (root!!.`val` == p!!.`val` || root!!.`val` == q!!.`val`) return root
+
+      val left = lowestCommonAncestor(root.left, p, q)
+      val right = lowestCommonAncestor(root.right, p, q)
+      
+      if (left == null) return right
+      if (right == null) return left
+      
+      return root
+    }
+}
