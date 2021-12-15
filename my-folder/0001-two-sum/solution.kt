a1f380df09c3dad8bbbdf6f2c97a6752c30ec4c1
Karan Dhillon <me@kdhillon.com>
2021-12-15 03:40:56 +0000

    [LeetCode Sync] Runtime - 401 ms (5.03%), Memory - 42.6 MB (100.00%)

--- my-folder/0001-two-sum/solution.kt
+++ my-folder/0001-two-sum/solution.kt
@@ -1,27 +1,15 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        val solution = IntArray(2)
-        var optimizationFlag = true
-        
-        nums.forEachIndexed { index, num ->
-            if(optimizationFlag) {
-                if(index == nums.size-1) {
-                    print("This should never be executed --__--")
-                }
-                else {
-                    for(i in index+1..nums.size-1) {
-                        if(optimizationFlag) {
-                            if(num + nums[i] == target) {
-                                solution[0] = index
-                                solution[1] = i
-                                optimizationFlag = false
-                            }
-                        }
-                    }
-                }
-            }
+      val map = HashMap<Int, Int>()
+      
+      nums.forEachIndexed { index, num ->
+        if (map.contains(target - num)) {
+          return intArrayOf(index, map.get(target - num)!!)
         }
         
-        return solution
+        map.put(num, index)
+      }
+      
+      return intArrayOf(0, 0)
     }
-}
+}  
