e1e7bc0b76d28c0366ce3f042e04113800d04f62
Karan Dhillon <me@kdhillon.com>
2021-12-29 13:36:06 +0000

    [LeetCode Sync] Runtime - 350 ms (6.47%), Memory - 37.1 MB (100.00%)

--- my-folder/0031-next-permutation/solution.kt
+++ my-folder/0031-next-permutation/solution.kt
@@ -1,42 +1,68 @@
 class Solution {
-    fun nextPermutation(nums: IntArray): Unit {
-        var i = nums.size - 2
-        
-        while (i >=0 && nums[i] >= nums[i+1]) {
-            i--
-        }
-        
-        var j = nums.size - 1
-        
-        if (i >= 0) {
-            while(j > i && nums[j] <= nums[i]) {
-                j--
-            }
-        
-            swap(nums, i, j)
-        }
-        
-        reverse(nums, i+1)
+  fun nextPermutation(nums: IntArray): Unit {
+    var l = 0
+    var r = nums.size - 1
+    var flag = true
+    
+    while (r > l && flag) {
+      if (nums[r] > nums[r-1]) {
+        flag = false
+        --r
+      } else {
+       --r 
+      }
     }
     
-    fun swap(nums: IntArray, i: Int, j: Int) {
-        val temp = nums[i]
-        nums[i] = nums[j]
-        nums[j] = temp
+    println("Index of first break: $r")
+    
+    if (flag) {
+      l = 0
+      r = nums.size - 1
+      
+      while (l <= r) {
+        val temp = nums[l]
+        nums[l] = nums[r]
+        nums[r] = temp
+      
+        ++l
+        --r
+      }
+      
+      return
     }
     
-    fun reverse(nums: IntArray, i: Int) {
-        var start = i
-        var end = nums.size - 1
-        
-        while (start < end) {
-            swap(nums, start, end)
-            start++
-            end--
-        }
+    val justGreatestIndex = findIndexOfJustGreatest(nums, nums[r], r+1)
+    
+    println("Index of just greatest: $justGreatestIndex")
+    
+    val temp = nums[justGreatestIndex]
+    nums[justGreatestIndex] = nums[r]
+    nums[r] = temp
+    
+    l = r + 1
+    r = nums.size - 1
+    
+    while (l <= r) {
+      val temp = nums[l]
+      nums[l] = nums[r]
+      nums[r] = temp
+      
+      ++l
+      --r
     }
+  }
+  
+  fun findIndexOfJustGreatest(nums: IntArray, target: Int, startIndex: Int): Int {
+    var output = 101
+    var result = 0
+    
+    for (i in startIndex until nums.size) {
+      if (nums[i] > target && nums[i] <= output) {
+        output = nums[i]
+        result = i
+      }
+    }
+    
+    return result
+  }
 }
-
-//1 2 4 5 2 1
-  //      i j  
-    //125124
