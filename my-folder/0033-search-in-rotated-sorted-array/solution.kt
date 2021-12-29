f28537333619f2149a51d4ac65339a426f333ead
Karan Dhillon <me@kdhillon.com>
2021-12-29 03:17:47 +0000

    [LeetCode Sync] Runtime - 172 ms (14.76%), Memory - 35.1 MB (100.00%)

--- my-folder/0033-search-in-rotated-sorted-array/solution.kt
+++ my-folder/0033-search-in-rotated-sorted-array/solution.kt
@@ -1,22 +1,20 @@
 class Solution {
-    fun search(nums: IntArray, target: Int): Int {
-      if (nums.isEmpty()) return -1
-      
-      val s = findSmallestNumberIndex(nums)
+    fun search(nums: IntArray, target: Int): Int {      
       var l = 0
-      var r = nums.size - 1      
-          
-      if (target >= nums[s] && target <= nums[r]) l = s
-      else r = s
+      var r = nums.size - 1
+      val s = findSmallestNumberIndex(nums)
       
+      if (target >= nums[s] && target <= nums[r]) l = s
+      else r = s - 1
+  
       while (l <= r) {
         val m = l + (r - l) / 2
         
         if (nums[m] == target) return m
-        if (nums[m] < target) l = m + 1
-        if (nums[m] > target) r = m - 1
+        else if (nums[m] < target) l = m + 1
+        else r = m -1
       }
-      
+
       return -1
     }
     
@@ -27,11 +25,8 @@ class Solution {
       while (l < r) {
         val m = l + (r - l) / 2
         
-        if (nums[m] > nums[r]) {
-          l = m + 1
-        } else {
-          r = m
-        }
+        if (nums[m] > nums[r]) l = m + 1
+        else if (nums[m] < nums[r]) r = m
       }
       
       return l
