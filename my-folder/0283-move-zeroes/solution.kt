60da2fcccb22ee8fe7eb90c1b3a32bfd1a488393
Karan Dhillon <me@kdhillon.com>
2021-12-29 09:49:19 +0000

    [LeetCode Sync] Runtime - 581 ms (5.00%), Memory - 64.5 MB (5.88%)

--- my-folder/0283-move-zeroes/solution.kt
+++ my-folder/0283-move-zeroes/solution.kt
@@ -1,31 +1,22 @@
 class Solution {
     fun moveZeroes(nums: IntArray): Unit {
-        // sortZerosWithBubbleSort(nums)
-        sortZerosWithIndex(nums)
-    }
-    
-    fun sortZerosWithIndex(nums: IntArray): Unit {
-        var index = 0
-        
-        for(i in 0 until nums.size) {
-            if (nums[i] != 0)
-                nums[index++] = nums[i]
+      var zeroCounter = 0
+      
+      nums.forEach { if (it == 0) ++zeroCounter }
+
+      var left = 0
+      var right = nums.size - zeroCounter
+      
+      for (i in 0 until nums.size) {
+        if (nums[i] != 0) {
+          nums[left] = nums[i]
+          left++
         }
-        
-        for (i in index until nums.size) {
-            nums[i] = 0
-        }
-    }
-    
-    fun sortZerosWithBubbleSort(nums: IntArray): Unit {
-         // Applying Bubble Sort
-        for (i in 0 until nums.size) {
-            for (j in 0 until nums.size-1) {
-                if (nums[j] == 0) {
-                    nums[j] = nums[j+1]
-                    nums[j+1] = 0
-                }
-            }
-        }
+      }
+      
+      while (right <= nums.size - 1) {
+        nums[right] = 0
+        right++
+      }
     }
 }
