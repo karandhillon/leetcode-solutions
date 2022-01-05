2c1c306e9ecaffaf36b43fac03de158ab0bc6ecc
Karan Dhillon <me@kdhillon.com>
2022-01-05 13:11:47 +0000

    [LeetCode Sync] Runtime - 520 ms (5.20%), Memory - 70.8 MB (5.20%)

--- my-folder/0680-valid-palindrome-ii/solution.kt
+++ my-folder/0680-valid-palindrome-ii/solution.kt
@@ -1,34 +1,25 @@
 class Solution {
+  var hasCharbeenRemoved = false
     fun validPalindrome(s: String): Boolean {
       var l = 0
       var r = s.length - 1
       
-      while (l <= r) {
-        if (s[l] != s[r]) {
-          return isPalindrome(s, l, r-1) || isPalindrome(s, l+1, r)
+      while (l < r) {
+        if (s[l] == s[r]) {
+          ++l
+          --r
+        } else {
+          if (!hasCharbeenRemoved) {
+            hasCharbeenRemoved = true
+            
+            val left = validPalindrome(s.substring(l, r))
+            val right = validPalindrome(s.substring(l+1, r+1))
+            
+            return if (left || right) true else false
+          } else {
+            return false
+          }
         }
-        
-        l++
-        r--
-      }
-      
-      return true
-    }
-    
-    fun isPalindrome(
-      s: String,
-      l: Int,
-      r: Int
-    ): Boolean {
-      var start = l
-      var end = r
-      
-      while (start <= end) {
-        if (s[start] != s[end])
-          return false
-        
-        start++
-        end--
       }
       
       return true
