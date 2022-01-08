f3bd0ab43118845d3008d71b1ebd2da201719961
Karan Dhillon <me@kdhillon.com>
2022-01-08 04:48:35 +0000

    [LeetCode Sync] Runtime - 302 ms (5.21%), Memory - 34.1 MB (100.00%)

--- my-folder/0144-binary-tree-preorder-traversal/solution.kt
+++ my-folder/0144-binary-tree-preorder-traversal/solution.kt
@@ -1,30 +1,17 @@
-/**
- * Example:
- * var ti = TreeNode(5)
- * var v = ti.`val`
- * Definition for a binary tree node.
- * class TreeNode(var `val`: Int) {
- *     var left: TreeNode? = null
- *     var right: TreeNode? = null
- * }
- */
 class Solution {
     fun preorderTraversal(root: TreeNode?): List<Int> {
-    val stack = Stack<TreeNode>()
-    val output = ArrayList<Int>()
-    
-    return if (root == null) output
-    else {
-        stack.push(root)
+        val output = ArrayList<Int>()
         
-        while (stack.isNotEmpty()) {
-            val popped = stack.pop()
-            output.add(popped.`val`)
-            popped.right?.let { stack.push(it) }
-            popped.left?.let { stack.push(it) }
-        }
+        dfs(root, output)
         
-        output
+        return output
     }
-}
+    
+    fun dfs(node: TreeNode?, output: ArrayList<Int>) {
+        if (node == null) return
+        
+        output.add(node.`val`)
+        dfs(node.left, output)
+        dfs(node.right, output)
+    }
 }
