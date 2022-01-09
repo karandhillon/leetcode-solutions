263d53ccf003b6e585835298e94818da63c3f5e5
Karan Dhillon <me@kdhillon.com>
2022-01-09 07:23:40 +0000

    [LeetCode Sync] Runtime - 401 ms (6.45%), Memory - 41 MB (100.00%)

---
+++ my-folder/0515-find-largest-value-in-each-tree-row/solution.kt
@@ -0,0 +1,38 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun largestValues(root: TreeNode?): List<Int> {
+      if (root == null) return emptyList()
+
+      val output = ArrayList<Int>()
+      
+      val queue = LinkedList<TreeNode>()
+      queue.offer(root)
+
+      while (queue.isNotEmpty()) {
+        val queueSize = queue.size
+        var max = Int.MIN_VALUE
+        
+        for (i in 0 until queueSize) {
+          val treeNode = queue.poll()
+          
+          max = Math.max(max, treeNode.`val`)
+          
+          treeNode.left?.let { queue.offer(it) }
+          treeNode.right?.let { queue.offer(it) }
+        }
+        
+        output.add(max)
+      }
+      
+      return output
+    }
+}
