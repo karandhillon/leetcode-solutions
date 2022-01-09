e7ebe6c75f62e1de42140569d29477293fa87fc9
Karan Dhillon <me@kdhillon.com>
2022-01-09 18:47:40 +0000

    [LeetCode Sync] Runtime - 278 ms (15.71%), Memory - 38.8 MB (100.00%)

--- my-folder/0215-kth-largest-element-in-an-array/solution.kt
+++ my-folder/0215-kth-largest-element-in-an-array/solution.kt
@@ -1,13 +1,15 @@
 class Solution {
     fun findKthLargest(nums: IntArray, k: Int): Int {
-      val pq = PriorityQueue<Int>()
+      val minPQ = PriorityQueue<Int>()
       
-      for (i in 0 until nums.size) {
-        pq.offer(nums[i])
+      nums.forEach { num ->
+        minPQ.offer(num)
         
-        if (pq.size > k) pq.poll()
+        if (minPQ.size > k) {
+          minPQ.poll()
+        }
       }
       
-      return pq.poll()
+      return minPQ.poll()
     }
 }
