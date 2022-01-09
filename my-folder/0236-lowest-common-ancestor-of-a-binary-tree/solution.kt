6c677a9424df22b4a4c37ff76edc6898a9e3c7c3
Karan Dhillon <me@kdhillon.com>
2022-01-09 18:40:21 +0000

    [LeetCode Sync] Runtime - 200 ms (6.33%), Memory - 38 MB (100.00%)

--- my-folder/0236-lowest-common-ancestor-of-a-binary-tree/solution.kt
+++ my-folder/0236-lowest-common-ancestor-of-a-binary-tree/solution.kt
@@ -1,24 +1,34 @@
 class Solution {
   var lca: TreeNode? = null
   
-  fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
-    recurseTree(root, p, q)
-    
-    return lca
-  }
-  
-  fun recurseTree(root: TreeNode?, p: TreeNode?, q: TreeNode?): Boolean {
-    if (root == null) return false
-    
-    val left = if (recurseTree(root.left, p, q)) 1 else 0
-    val right = if (recurseTree(root.right, p, q)) 1 else 0
-    
-    val mid = if (root.`val` == p!!.`val` || root.`val` == q!!.`val`) 1 else 0
-    
-    if (left + right + mid == 2) {
-      lca = root
+    fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+      if (root == null) return null
+      
+      helper(root, p, q)
+      
+      return lca
     }
     
-    return left + right + mid > 0
-  }
+    fun helper(node: TreeNode?, p: TreeNode?, q: TreeNode?): Boolean {
+      if (node == null) return false
+      
+      val isNodeMatch = if (node.`val` == p!!.`val` || node.`val` == q!!.`val`) true else false
+      val isLeftMatch = helper(node.left, p, q)
+      val isRightMatch = helper(node.right, p, q)
+      
+      if (isLeftMatch && isRightMatch) {
+        lca = node
+        return true
+      }
+      
+      if (isNodeMatch && (isLeftMatch || isRightMatch)) {
+        lca = node
+      }
+      
+      if (isLeftMatch || isRightMatch || isNodeMatch) {
+        return true
+      }
+
+      return false
+    }
 }
