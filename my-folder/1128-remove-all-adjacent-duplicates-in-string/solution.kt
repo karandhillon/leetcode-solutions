4d4b6fd1cf4bca0c857098112d3391ca6ccb2e09
Karan Dhillon <me@kdhillon.com>
2022-01-09 23:13:40 +0000

    [LeetCode Sync] Runtime - 603 ms (5.43%), Memory - 59.5 MB (5.43%)

--- my-folder/1128-remove-all-adjacent-duplicates-in-string/solution.kt
+++ my-folder/1128-remove-all-adjacent-duplicates-in-string/solution.kt
@@ -1,18 +1,22 @@
 class Solution {
-  fun removeDuplicates(s: String): String {
-    val stack = Stack<Char>()
-    
-    for (i in 0 until s.length) {
-      if (stack.isNotEmpty() && stack.peek() == s[i]) stack.pop()
-      else stack.push(s[i])
+    fun removeDuplicates(s: String): String {
+      val stack = Stack<Char>()
+      
+      for (i in 0 until s.length) {
+        if (stack.isEmpty()) {
+          stack.push(s[i])
+        }
+        else {
+          if (stack.peek() == s[i]) stack.pop()
+          else stack.push(s[i])
+        }
+      }
+      
+      val sb = StringBuilder()
+      while (stack.isNotEmpty()) {
+        sb.append(stack.pop())
+      }
+      
+      return sb.toString().reversed()
     }
-    
-    val sb = StringBuilder()
-    
-    while(stack.isNotEmpty()) {
-      sb.append(stack.pop())
-    }
-    
-    return sb.toString().reversed()
-  }
 }
