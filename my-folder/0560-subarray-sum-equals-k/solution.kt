7d58263fed05f22a031e9deb8b24e91e018dc49b
Karan Dhillon <me@kdhillon.com>
2022-01-10 18:26:30 +0000

    [LeetCode Sync] Runtime - 617 ms (17.75%), Memory - 69.1 MB (5.22%)

--- my-folder/0560-subarray-sum-equals-k/solution.kt
+++ my-folder/0560-subarray-sum-equals-k/solution.kt
@@ -1,15 +1,18 @@
 class Solution {
     fun subarraySum(nums: IntArray, k: Int): Int {
-      val map = HashMap<Int, Int>().apply { put(0, 1) }
-      var counter = 0
-      var sum = 0
-      
-      nums.forEach { num ->
-        sum += num
-        counter += map.getOrElse(sum - k) { 0 }
-        map.put(sum, map.getOrElse(sum) { 0 } + 1)
-      }
-      
-      return counter
+        val map = HashMap<Int, Int>()
+        map.put(0, 1)
+        
+        var counter = 0
+        var prefixSum = 0
+        nums.forEach { num ->
+            prefixSum += num
+            
+            if (map.contains(prefixSum-k)) counter += map.get(prefixSum-k)!!
+            
+            map.put(prefixSum, map.getOrElse(prefixSum) {0} + 1)
+        }
+           
+        return counter
     }
 }
