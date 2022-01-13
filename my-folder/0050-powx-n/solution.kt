9be048b8ede4add0cee47a0870cfc85bc4f79061
Karan Dhillon <me@kdhillon.com>
2022-01-13 00:19:30 +0000

    [LeetCode Sync] Runtime - 258 ms (5.00%), Memory - 35.1 MB (100.00%)

--- my-folder/0050-powx-n/solution.kt
+++ my-folder/0050-powx-n/solution.kt
@@ -1,5 +1,12 @@
 class Solution {
     fun myPow(x: Double, n: Int): Double {
-        return Math.pow(x, n.toDouble())
+      return if (n < 0) fastPower(1/x, -n) else fastPower(x, n)
+    }
+    
+    fun fastPower(x: Double, n: Int): Double {
+      if (n == 0) return 1.0
+      
+      val half = fastPower(x, n/2)
+      return if (n%2 == 0) half*half else half*half*x
     }
 }
