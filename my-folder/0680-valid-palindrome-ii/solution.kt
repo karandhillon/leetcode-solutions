6b8a2ccf374826292e85537c5e4900e603de1169
Karan Dhillon <me@kdhillon.com>
2022-01-14 05:22:21 +0000

    [LeetCode Sync] Runtime - 533 ms (5.20%), Memory - 69.2 MB (5.20%)

--- my-folder/0680-valid-palindrome-ii/solution.kt
+++ my-folder/0680-valid-palindrome-ii/solution.kt
@@ -1,9 +1,6 @@
 class Solution {
-  var isCharDeleted = false
-  
-    fun validPalindrome(s: String): Boolean {
-      if (s.isEmpty()) return true
-      
+  var hasSingleCharBeenDeleted = false
+    fun validPalindrome(s: String): Boolean {  
       var l = 0
       var r = s.length - 1
       
@@ -12,12 +9,11 @@ class Solution {
           ++l
           --r
         } else {
-          if (isCharDeleted) return false
-          else {
-            isCharDeleted = true
-            return validPalindrome(s.substring(l, r)) || validPalindrome(s.substring(l+1, r+1))
-          }
-        }      
+          if (hasSingleCharBeenDeleted) return false
+          
+          hasSingleCharBeenDeleted = true
+          return validPalindrome(s.substring(l, r)) || validPalindrome(s.substring(l+1, r+1))
+        }
       }
       
       return true
