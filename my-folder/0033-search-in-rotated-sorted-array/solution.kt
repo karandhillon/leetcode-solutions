5af37f0f9a4e8f3bc19c31de785f2d0f947d98e0
Karan Dhillon <me@kdhillon.com>
2022-01-16 00:31:47 +0000

    [LeetCode Sync] Runtime - 275 ms (14.76%), Memory - 35.2 MB (100.00%)

--- my-folder/0033-search-in-rotated-sorted-array/solution.kt
+++ my-folder/0033-search-in-rotated-sorted-array/solution.kt
@@ -1,34 +1,55 @@
 class Solution {
-    fun search(nums: IntArray, target: Int): Int {      
-      var l = 0
-      var r = nums.size - 1
-      val s = findSmallestNumberIndex(nums)
-      
-      if (target >= nums[s] && target <= nums[r]) l = s
-      else r = s - 1
-  
-      while (l <= r) {
-        val m = l + (r - l) / 2
-        
-        if (nums[m] == target) return m
-        else if (nums[m] < target) l = m + 1
-        else r = m -1
+    fun search(nums: IntArray, target: Int): Int {
+      if (nums.size == 1) {
+        return if (nums[0] == target) 0 else -1
       }
-
-      return -1
-    }
-    
-    fun findSmallestNumberIndex(nums: IntArray): Int {
+      
       var l = 0
       var r = nums.size - 1
       
       while (l < r) {
-        val m = l + (r - l) / 2
+        val m = l + (r - l ) /2
         
         if (nums[m] > nums[r]) l = m + 1
-        else if (nums[m] < nums[r]) r = m
+        else r = m
       }
       
-      return l
+      var s = r
+      
+      l = 0
+      r = nums.size-1
+      
+      if (target <= nums[r]) {
+        l = s
+        r = nums.size - 1
+        
+        while (l <= r) {
+          val m = l + (r - l) / 2
+
+          if (nums[m] == target) return m
+          
+          if (nums[m] < target) l = m + 1
+          else r = m - 1
+        }
+        
+        return -1
+      }
+      else {
+        l = 0
+        r = s-1
+        
+        while (l <= r) {
+          val m = l + (r - l) / 2
+          
+          if (nums[m] == target) return m
+          
+          if (nums[m] < target) l = m + 1
+          else r = m - 1
+        }
+        
+        return -1
+      }
+      
+      return -1
     }
 }
