d45286b6406fa263bcbc8bd8e04954d8e5040d4e
Karan Dhillon <me@kdhillon.com>
2022-01-23 08:50:04 +0000

    [LeetCode Sync] Runtime - 889 ms (5.03%), Memory - 73.7 MB (5.18%)

--- my-folder/0056-merge-intervals/solution.kt
+++ my-folder/0056-merge-intervals/solution.kt
@@ -1,5 +1,9 @@
 class Solution {
     fun merge(intervals: Array<IntArray>): Array<IntArray> {
+      if (intervals.size == 0) return arrayOf<IntArray>()
+      
+      val output = ArrayList<IntArray>()
+      
       val c = object: Comparator<IntArray> {
         override fun compare(o1: IntArray, o2: IntArray): Int {
           return if (o1[0] > o2[0]) 1
@@ -7,16 +11,23 @@ class Solution {
           else 0
         }
       }
-      val newIntervals: List<IntArray> = intervals.sortedWith(c)
+      intervals.sortWith(c)
       
-      val output = ArrayList<IntArray>()
-      output.add(newIntervals[0])
-      
-      for (i in 1 until newIntervals.size) {
-        if (output.last()[1] >= newIntervals[i][0]) {
-          output.last()[1] = Math.max(output.last()[1], newIntervals[i][1])
-        } else {
-          output.add(newIntervals[i])
+      for (interval in intervals) {
+        if (output.isEmpty()) output.add(interval)
+        else {
+          val last = output.last()
+          val ls = last.get(0)
+          val le = last.get(1)
+          
+          val s = interval.get(0)
+          val e = interval.get(1)
+          
+          if (le >= s) {
+            output.removeAt(output.size - 1)
+            output.add(intArrayOf(ls, Math.max(le, e)))
+          } 
+          else output.add(interval)
         }
       }
       
