c7099c897f55417c1aee5c601c754662f325081b
Karan Dhillon <me@kdhillon.com>
2025-12-02 18:31:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 49.6 MB (54.33%)

--- my-folder/0792-binary-search/solution.kt
+++ my-folder/0792-binary-search/solution.kt
@@ -1,18 +1,25 @@
 class Solution {
     fun search(nums: IntArray, target: Int): Int {
-      var l = 0
-      var r = nums.size-1
-      
-      while (l <= r) {
-        val m = l + (r-l)/2
-        
-        when {
-          nums[m] == target -> return m
-          nums[m] < target -> l = m + 1
-          target < nums[m] -> r = m - 1
+        var l = 0
+        var r = nums.size - 1
+
+        while (l <= r) {
+            val m = (l+r)/2
+
+            if (nums[m] == target) {
+                return m
+            }
+
+            if (nums[m] < target) {
+                l = m + 1
+            }
+
+            if (nums[m] > target) {
+                r = m - 1
+            }
         }
-      }
-      
-      return -1
+
+        // target not found
+        return -1
     }
 }
