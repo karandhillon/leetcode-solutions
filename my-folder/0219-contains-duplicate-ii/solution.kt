bb6ac5a656569c6d958dbedd1811c493997ca9f1
Karan Dhillon <me@kdhillon.com>
2025-12-03 12:57:35 +0000

    [LeetCode Sync] Runtime - 36 ms (53.09%), Memory - 66.6 MB (24.74%)

--- my-folder/0219-contains-duplicate-ii/solution.kt
+++ my-folder/0219-contains-duplicate-ii/solution.kt
@@ -1,16 +1,18 @@
 class Solution {
     fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
-      val map = HashMap<Int, Int>()
-      
-      for (i in 0 until nums.size) {
-        if (
-          map.contains(nums[i]) && 
-          Math.abs(map.get(nums[i])!! - i) <= k
-        ) return true
-        
-        map.put(nums[i], i)
-      }
-      
-      return false
+        val hashMap = HashMap<Int, Int>()
+
+        nums.forEachIndexed { index, value -> 
+            if (
+                hashMap.contains(value) &&
+                Math.abs(hashMap.get(value)!! - index) <= k
+            ) {
+                return true
+            }
+
+            hashMap.put(value, index)
+        }
+
+        return false
     }
 }
