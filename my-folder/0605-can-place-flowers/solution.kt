9da9ff0e1d3dc8838fefeb154be89d6db085904c
Karan Dhillon <me@kdhillon.com>
2025-12-17 15:18:13 +0000

    [LeetCode Sync] Runtime - 2 ms (61.32%), Memory - 47.7 MB (95.60%)

--- my-folder/0605-can-place-flowers/solution.kt
+++ my-folder/0605-can-place-flowers/solution.kt
@@ -1,18 +1,17 @@
 class Solution {
     fun canPlaceFlowers(flowerbed: IntArray, n: Int): Boolean {
-      var plantsLeft = n 
-      
-      for (i in 0 until flowerbed.size) {
-        if (
-          flowerbed.getOrElse(i - 1) { 0 } == 0 &&
-          flowerbed.getOrElse(i + 1) { 0 } == 0 &&
-          flowerbed[i] == 0  
-        ) {
-          flowerbed[i] = 1
-          --plantsLeft
-        } 
-      }
-      
-      return if (plantsLeft > 0) false else true
+        var counter = 0
+
+        for (i in 0..flowerbed.size - 1) {
+            val prev = flowerbed.getOrElse(i - 1) { 0 }
+            val next = flowerbed.getOrElse(i + 1) { 0 }
+
+            if (prev == 0 && next == 0 && flowerbed[i] == 0) {
+                flowerbed[i] = 1
+                ++counter
+            }
+        }
+
+        return counter >= n 
     }
-}  
+}
